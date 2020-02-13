6c0c7311bba4e8e50b6c8ea8d2ca8fb505cffae2
Paavan Chopra <paavan012chopra@gmail.com>
2020-02-13 15:58:52 -0500

    add method to get self and neighbors

--- python/gameoflife/GameOfLife.py
+++ python/gameoflife/GameOfLife.py
@@ -7,3 +7,12 @@ class GameOfLife:
     if self.live_cords:
       return True
     return False
+
+  def getNine(self, x, y):
+    result = set()
+    for i in range(x-1, x+2):
+      for j in range(y-1, y+2):
+        result.add((i,j))
+    return result
+
+
--- python/tests/test_GameOfLife.py
+++ python/tests/test_GameOfLife.py
@@ -7,3 +7,9 @@ def test_isAlive():
 def test_isDead():
     game = gameoflife.GameOfLife(set())
     assert not game.isAlive()
+
+def test_getNine():
+    game = gameoflife.GameOfLife(set())
+    neighbors = game.getNine(1,1)
+    print(neighbors)
+    assert neighbors == set([(0,0), (0,1) , (0,2), (1,0), (1,1) , (1,2), (2,0), (2,1) , (2,2)])
